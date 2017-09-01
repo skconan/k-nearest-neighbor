833c95000e304c5004b5d768a57551759828335e
skconan <supakit.kr@gmail.com>
2017-09-01 07:38:59 +0700

    created run function and edit find_feature

--- create_data_set.py
+++ create_data_set.py
@@ -3,18 +3,20 @@ import cv2
 
 class CreateDataSet():
     def __init__(self):
-        self.imagesPath = 'C:\\Users\\skconan\\Desktop\\classification_rect\\images\\'
+        self.imagesPath = 'C:\\Users\\skconan\\Desktop\\classification_rectangle\\images\\rectangle_training\\'
         self.filePath = 'C:\\Users\\skconan\\Desktop\\classification_rectangle\\src_code\\'
         self.font = cv2.FONT_HERSHEY_SIMPLEX
         self.imageW = 300
         self.imageH = 300
-        self.minCntArea = 100
-        self.maxCntArea = 500
+        self.minCntArea = 500
+        self.maxCntArea = 150000
         self.fileType = '.txt'
+        self.noOftraning = 8
+
     def create_file(self, name, dataList):
         name += self.fileType
         while True:
-            name = self.filePath+name
+            name = self.filePath + name
             try:
                 f = open(name, 'r+')
                 cmd = input('Do you want overwrite it (y/n) ')
@@ -33,39 +35,46 @@ class CreateDataSet():
         print(text)
         f.write(text)
         f.close()
-        print('Finish')
+        print('Created ' + str(name))
 
-    def find_features(self,img):
+    def find_features(self, img):
         r, c, ch = img.shape
         img = cv2.resize(img, (self.imageW, self.imageH))
         res = img.copy()
         gray = cv2.cvtColor(img.copy(), cv2.COLOR_BGR2GRAY)
-        ret, th = cv2.threshold(gray.copy(), 250, 255, 0)
+        ret, th = cv2.threshold(gray.copy(), 200, 255, 0)
         _, cnts, h = cv2.findContours(th, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-        cntArea = cv2.contourArea(cnts)
-        for c in cnts:
-            if  cntArea < self.minCntArea or cntArea > self.maxCntArea:
-                continue
-            peri = cv2.arcLength(c, True)
-            approx = cv2.approxPolyDP(c, 0.04 * peri, True)
-            sides = len(approx)
+        print(len(cnts))
+        cnt = cnts[-1]
+        cntArea = cv2.contourArea(cnt)
+        print(cntArea)
+        if cntArea < self.minCntArea or cntArea > self.maxCntArea:
+            return
+        peri = cv2.arcLength(cnt, True)
+        approx = cv2.approxPolyDP(cnt, 0.04 * peri, True)
+        sides = len(approx)
+        x, y, w, h = cv2.boundingRect(cnt)
+        areaRatio = float(cntArea) / (w * h)
+        cx, cy = int(x + w / 2), int(y + h / 2)
+        cv2.drawContours(res, cnt, -1, (0, 255, 0), 3)
+        cv2.drawContours(res, approx, -1, (0, 0, 205), 5)
+        cv2.putText(res, str(int(peri)) + " " + str(sides), (cx, cy),
+                    self.font, 0.5, (0, 50, 25), 1, cv2.LINE_AA)
 
-            x, y, w, h = cv2.boundingRect(c)
-            cx, cy = int(x + w / 2), int(y + h / 2)
-
-            cv2.drawContours(res, c, -1, (0, 255, 0), 3)
-            cv2.drawContours(res, approx, -1, (0, 0, 205), 5)
-            cv2.putText(resCnt, str(int(peri)) + " " + str(sides), (cx, cy),
-                        font, 0.5, (0, 50, 25), 1, cv2.LINE_AA)
-
-        cv2.imshow('result', resCnt)
+        cv2.imshow('result', res)
         cv2.waitKey(0)
-        return (sides,areaRatio)
+        return [sides, areaRatio]
 
-        
-    def run():
-        pass      
-if __name__=='__main__':
+    def run(self):
+        dataList = []
+        for no in range(self.noOftraning):
+            img = cv2.imread(self.imagesPath+"0"+str(no)+".jpg")
+            data = self.find_features(img)
+            print( data)
+            dataList.append(data)
+        self.create_file('trainingSet1',dataList)
+if __name__ == '__main__':
     cds = CreateDataSet()
-    data = [[121,32],[621,72],[1231,323]]
-    cds.create_file('test',data)
+    # data = [[121, 32], [621, 72], [1231, 323]]
+    # cds.create_file('test', data)
+    cds.run()
